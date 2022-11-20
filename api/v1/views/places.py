e8b81ef1934dadcee4c8bb5790a5b6f0ea7627ed
Coder1967 <nosakhareaghedo42@gmail.com>
2022-11-20 21:07:56 +0000

    completing more tasks

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,6 +5,8 @@ from flask import request
 from flask import abort
 from flask import jsonify
 from . import Place
+from . import State
+from . import Amenity
 from . import User
 from . import City
 from . import app_views
@@ -63,18 +65,41 @@ def post_place(city_id):
     return jsonify(new_place.to_dict()), 201
 
 
-@app_views.route('/places/<place_id>', methods=['PUT'])
-def put_place(place_id):
-    """ updates place """
-    place = storage.get(Place, place_id)
-    req = request.get_json()
-    restricted = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
-    if place is None:
-        abort(404)
-    if req is None:
-        abort(400, 'Not a JSON')
-    for key in req.keys():
-        if key not in restricted:
-            place.__dict__[key] = req[key]
-    place.save()
-    return jsonify(place.to_dict())
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def post_search():
+    """searches for a particular place"""
+    if request.get_json() is not None:
+        req = request.get_json()
+        states = req.get('states', [])
+        cities = req.get('cities', [])
+        amenities = req.get('amenities', [])
+        amenity_objects = []
+        for amenity_id in amenities:
+            amenity = storage.get(Amenity, amenity_id)
+            if amenity:
+                amenity_objects.append(amenity)
+        if states == cities == []:
+            places = storage.all(Place).values()
+        else:
+            places = []
+            for state_id in states:
+                state = storage.get(State, state_id)
+                state_cities = state.cities
+                for city in state_cities:
+                    if city.id not in cities:
+                        cities.append(city.id)
+            for city_id in cities:
+                city = storage.get(City, city_id)
+                for place in city.places:
+                    places.append(place)
+        confirmed_places = []
+        for place in places:
+            place_amenities = place.amenities
+            confirmed_places.append(place.to_dict())
+            for amenity in amenity_objects:
+                if amenity not in place_amenities:
+                    confirmed_places.pop()
+                    break
+        return jsonify(confirmed_places)
+    else:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ holds class User"""
 import models
+import hashlib
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
@@ -26,4 +27,14 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if kwargs.get('password') is not None:
+            pwd = kwargs['password']
+            del kwargs['password']
+            self.__secure_password(pwd)
         super().__init__(*args, **kwargs)
+
+    def __secure_password(self, pwd):
+        """ encrypts user password to md5"""
+        secure = hashlib.md5()
+        secure.update(pwd.encode("utf-8"))
+        self.password = secure.hexdigest()
