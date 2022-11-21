6cb9bf210350cc8d74bfe6f96dc4fb0354d35096
Coder1967 <nosakhareaghedo42@gmail.com>
2022-11-21 20:00:48 +0000

    fixing up places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -14,7 +14,7 @@ from flask import make_response
 from . import storage
 
 
-@app_views.route('/cities/<city_id>/places', methods=["GET"],
+@app_views.route('/cities/<string:city_id>/places', methods=["GET"],
                  strict_slashes=False)
 def get_places(city_id):
     """ gets all places in a city"""
@@ -29,7 +29,7 @@ def get_places(city_id):
     return jsonify(places)
 
 
-@app_views.route('/places/<place_id>', methods=['GET'],
+@app_views.route('/places/<string:place_id>', methods=['GET'],
                  strict_slashes=False)
 def get_place(place_id):
     """ retrives a place instance using its id"""
@@ -39,7 +39,7 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'],
+@app_views.route('/places/<string:place_id>', methods=['DELETE'],
                  strict_slashes=False)
 def del_place(place_id):
     """deletes an instance of place"""
@@ -52,7 +52,7 @@ def del_place(place_id):
     return jsonify({})
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'],
+@app_views.route('/cities/<string:city_id>/places', methods=['POST'],
                  strict_slashes=False)
 def post_place(city_id):
     """adds new place"""
@@ -62,9 +62,27 @@ def post_place(city_id):
         abort(404)
     if req is None:
         abort(400, 'Not a JSON')
-    if req.get('name') is None:
+    if req.get('user_id') is None:
         abort(400, 'Missing user_id')
     req['city_id'] = city.id
     new_place = Place(**req)
     new_place.save()
     return jsonify(new_place.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_place(place_id):
+    """ updates a place object"""
+    req = request.get_json()
+    place = storage.get(Place, place_id)
+    restricted = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
+    
+    if place is None:
+        abort(404)
+    if req is None:
+        abort(400, 'Not a JSON')
+    for key in req.keys():
+        if key not in restricted:
+            place.__dict__[key] = req[key]
+    return jsonify(place.to_dict())
